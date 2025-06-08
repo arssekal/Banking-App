6a96f827c63115ed2fb478ca8c2901307137c7d6
arssekal <arssekall@gmail.com>
2025-06-08 12:02:07 +0100

    addign the project structure for the first time

---
+++ src/main/java/com/arssekal/bancking_app/BanckingAppApplication.java
@@ -0,0 +1,13 @@
+package com.arssekal.bancking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanckingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanckingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arssekal/bancking_app/BanckingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arssekal.bancking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanckingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
